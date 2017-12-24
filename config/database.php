ae5975dadcd3cb031294b408b07d23456af59711
WaitMoonMan <1033404553@qq.com>
2017-12-24 16:15:33 +0800

    :tada:  init

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'forge',
+            'username' => 'forge',
+            'password' => '',
+            'charset' => 'utf8mb4',
+        ],
+];
---
+++ src/DB.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Waitmoonman\Database;
+
+class DB
+{
+
+}
