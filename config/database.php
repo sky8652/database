72c19ed35fd734d60c299a8cc659d16b36e587cf
DavidNineRoc <1033404553@qq.com>
2018-01-12 22:38:17 +0800

    ':hammer:添加测试和样式修改'

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ return [
     'driver' => 'mysql',
     'host' => '127.0.0.1',
     'port' => '3306',
-    'database' => 'test',
+    'database' => 'monday_shop',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8mb4',
